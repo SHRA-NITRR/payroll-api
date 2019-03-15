d5e76a8715aae8af9cdc685dfe2f129be90c158a
Akash Gautam <gautamakash91@gmail.com>
2019-03-15 13:59:48 +0530

    async trial

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var Connection = require('express').Connection;
 var Request = require('express').Request;
 var sql = require('mssql');
 var multer = require('multer');
-
+var async = require("async");
 
 app.set('port', (process.env.PORT || 8008));
 app.use(cors());
@@ -21,7 +21,7 @@ var config = {
    user: 'sa',
    password: '12345',
    server: 'localhost',
-   database: 'VYPAK'
+   database: 'VYPACK'
 }
 
 var companyroute = require('./routes/company.js');
@@ -107,6 +107,94 @@ module.exports = connection;
 
 //WELCOME API
 app.get('/', function (req, res) {
+   res.send("WELCOME TO PAYROLL APP API'S");
+});
+
+
+app.get('/array', function (req, res) {
+   var a = [{
+      "Branch_Name": "BbbsrPATIA",
+      "Branch_Address": "Patia",
+      "Branch_Address2": "Infocity",
+      "Branch_PhoneNo": "123",
+      "Branch_Person_Name": "Soumya",
+      "Branch_Email": "soumya@gmail.com",
+      "Branch_PF_Group": 1,
+      "Branch_PT_Group": 22,
+      "Branch_ESI_Group": 7676,
+      "Branch_Id": 1
+   }, {
+      "Branch_Name": "BbbsrPATIA",
+      "Branch_Address": "Patia",
+      "Branch_Address2": "Infocity",
+      "Branch_PhoneNo": "123",
+      "Branch_Person_Name": "Soumya",
+      "Branch_Email": "soumya@gmail.com",
+      "Branch_PF_Group": 1,
+      "Branch_PT_Group": 22,
+      "Branch_ESI_Group": 7676,
+      "Branch_Id": 1
+   }]
+   // var b = [];
+   // for(var i=0;i<a.length;i++){
+   //    var c = [];
+   //    c.push(a[i].branch, a[i].name);
+   //    b.push(c);
+   // }
+   // var d = [];
+   // d.push(b);
+   // console.log(d);
+   const transaction = new sql.Transaction();
+   var data_added = true;
+   transaction.begin((err) => {
+      async.eachSeries(a, function iteratee(doc, callback) {
+         // console.log(filePath);
+
+
+         request = new sql.Request(transaction);
+
+         request.input('Operation', 'INSERT');
+
+         request.input('Branch_Name', doc.Branch_Name);
+
+         request.input('Branch_Address', doc.Branch_Address);
+
+         request.input('Branch_Address2', doc.Branch_Address2);
+
+         request.input('Branch_PhoneNo', parseInt(doc.Branch_PhoneNo));
+
+         request.input('Branch_Person_Name', doc.Branch_Person_Name);
+
+         request.input('Branch_Email', doc.Branch_Email);
+
+         request.input('Branch_PF_Group', parseInt(doc.Branch_PF_Group));
+
+         request.input('Branch_PT_Group', parseInt(doc.Branch_PT_Group));
+
+         request.input('Branch_ESI_Group', parseInt(doc.Branch_ESI_Group));
+
+         request.execute('PROC_COMPANY_BRANCH', function (errr, rec) {
+
+            if (errr) {
+               console.log(errr);
+               data_added = false;
+               console.log(data_added);
+            }
+            else {
+               // setTimeout(function () {
+               console.log("hello");
+               // }, 500);
+            }
+         });
+
+
+      });
+
+
+
+
+   });
+   console.log(data_added);
    res.send("WELCOME TO PAYROLL APP API'S");
 });
 
@@ -269,24 +357,15 @@ app.post('/view2', function (req, res) {
          });
          console.log(rec.recordsets[0]);
          //res.json({ status: true, result:resultArry});
-
-
-
       }
    });
 });
 
 
 
-
-
-
-
-
-
 //API  TYPE
 
-app.post('/view', function (req, res) {
+app.post('/viewalldetails', function (req, res) {
    var resultArry = [];
    var request = new sql.Request(connection);
    request.input('Operation', 'SELECTBYID28');
